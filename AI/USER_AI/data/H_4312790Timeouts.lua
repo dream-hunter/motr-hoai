a75908bf7561b995ecf6295dd7e7d95be1a3bfa6
dream-hunter <cinematik@mail.ru>
2020-08-12 21:55:08 +0700

    up

--- AI/USER_AI/data/H_4312790Timeouts.lua
+++ AI/USER_AI/data/H_4312790Timeouts.lua
@@ -1,13 +1,13 @@
-MagTimeout=598000
+MagTimeout=608000
 SOffensiveTimeout=0
 SDefensiveTimeout=0
 SOwnerBuffTimeout=0
-GuardTimeout=8276634
-QuickenTimeout=8836868
+GuardTimeout=7335853
+QuickenTimeout=8039993
 OffensiveOwnerTimeout=0
 DefensiveOwnerTimeout=0
 OtherOwnerTimeout=0
 ShouldStandby=0
-RegenTick[1]=19
+RegenTick[1]=10
 MySpheres=0
 EleanorMode=0
